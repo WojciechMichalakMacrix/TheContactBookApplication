7627ea060822fb095262a461e27685c8f7ffeda7
Wojciech Michalak <wojciech.michalak@macrix.pl>
2023-11-17 11:38:16 +0100

    Added "Edit" button

--- YPP/source/TheContactBookApplication/Models/ContactModel.cs
+++ YPP/source/TheContactBookApplication/Models/ContactModel.cs
@@ -1,25 +1,35 @@
-﻿namespace TheContactBookApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheContactBookApplication.Models
 {
     public class ContactModel : IContactModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required]
         public string LastName { get; set; } = string.Empty;
 
+        [Required]
         public string StreetName { get; set; } = string.Empty;
 
+        [Required]
         public int HouseNumber { get; set; }
 
         public int? ApartmentNumber { get; set; }
 
+        [Required]
         public string PostalCode { get; set; } = string.Empty;
 
+        [Required]
         public string Town { get; set; } = string.Empty;
 
+        [Required]
         public int PhoneNumber { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
 
     }
