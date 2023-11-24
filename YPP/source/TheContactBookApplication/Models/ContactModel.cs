c89f374b4908b5c34f721209b28a420a482b0a5c
Wojciech Michalak <wojciech.michalak@macrix.pl>
2023-11-24 15:37:53 +0100

    FINAL COMMIT

--- YPP/source/TheContactBookApplication/Models/ContactModel.cs
+++ YPP/source/TheContactBookApplication/Models/ContactModel.cs
@@ -16,19 +16,21 @@ namespace TheContactBookApplication.Models
         public string StreetName { get; set; } = string.Empty;
 
         [Required]
+        [Range(1, 10000, ErrorMessage = "Provide a proper House Number.")]
         public int HouseNumber { get; set; }
 
+        [Range(1, 10000, ErrorMessage = "Provide a proper Apartment Number.")]
         public int? ApartmentNumber { get; set; }
 
         [Required]
-        [StringLength(6, MinimumLength = 6, ErrorMessage = "Provide PostalCode of format ## - ###.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Provide Postal Code of format ## - ###.")]
         public string PostalCode { get; set; } = string.Empty;
 
         [Required]
         public string Town { get; set; } = string.Empty;
 
         [Required]
-        [Range(100000000, 999999999, ErrorMessage = "Provide PhoneNumber that contains 9 digits.")]
+        [Range(100000000, 999999999, ErrorMessage = "Provide Phone Number that contains 9 digits.")]
         public int PhoneNumber { get; set; }
 
         [Required]
